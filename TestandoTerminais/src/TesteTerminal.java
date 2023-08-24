d7371ee7f0354e65bc14ce5edb3d6f4d773f352f
jorginn2 <jorgevinicius0911@hotmail.com>
2023-08-24 15:32:20 -0300

    alterando classe TesteTerminal

--- TestandoTerminais/src/TesteTerminal.java
+++ TestandoTerminais/src/TesteTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class TesteTerminal{
 
+       public static void main(String[] args) throws InterruptedException{
+        
+         String nome;
 
-    
+            System.out.println("Teste terminal, fui executado pelo ms-dos");
+             Scanner entrada= new Scanner(System.in);
+             System.out.println("Insira seu nome: ");
+             nome = entrada.nextLine();
+              Thread.sleep(2000);  
+             System.out.println(nome);
+            
+             }
+
 }
