63730f5160d08524ddfc06960423f4f69d681b1c
jorginn2 <jorgevinicius0911@hotmail.com>
2023-08-24 17:47:42 -0300

    criando minhaClasse

--- TestandoTerminais/src/TesteTerminal.java
+++ TestandoTerminais/src/TesteTerminal.java
@@ -3,15 +3,15 @@ public class TesteTerminal{
 
        public static void main(String[] args) throws InterruptedException{
         
-         String nome;
+        Scanner entrada= new Scanner(System.in);
+         
+          String nome;
 
             System.out.println("Teste terminal, fui executado pelo ms-dos");
-             Scanner entrada= new Scanner(System.in);
-             System.out.println("Insira seu nome: ");
-             nome = entrada.nextLine();
-              Thread.sleep(2000);  
-             System.out.println(nome);
-            
-             }
-
+             
+              System.out.println("Insira seu nome: ");
+                nome = entrada.nextLine();
+                  Thread.sleep(2000);  
+                    System.out.println(nome);
+      }
 }
